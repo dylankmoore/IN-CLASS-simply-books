f63d6e987d2f7129a0b97c3c989d49f5bc5b7e08
Dylan Moore <dylankmoore@gmail.com>
2023-10-12 02:41:30 -0500

    added author images in details

--- pages/author/[firebaseKey].js
+++ pages/author/[firebaseKey].js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import { Button } from 'react-bootstrap';
+import Card from 'react-bootstrap/Card';
 import { viewAuthorDetails } from '../../api/mergedData';
 import BookCard from '../../components/BookCard';
 
@@ -15,6 +16,9 @@ export default function ViewAuthor() {
 
   return (
     <div className="mt-5 d-flex flex-wrap">
+      <div className="d-flex flex-column">
+        <Card.Img variant="top" src={authorDetails.image} alt={authorDetails.email} style={{ height: '400px' }} />
+      </div>
       <div className="text-white ms-5 details">
         <h5>
           {authorDetails.first_name} {authorDetails.last_name}
@@ -23,7 +27,7 @@ export default function ViewAuthor() {
         </h5>
         <Link href={`/author/edit/${authorDetails.firebaseKey}`} passHref>
           <Button variant="info">EDIT</Button>
-        </Link>
+        </Link><hr />
         <div className="mt-5 d-flex flex-wrap">
           { authorDetails.books?.map((book) => <BookCard bookObj={book} onUpdate={viewAuthorDetails} />)}
         </div>
